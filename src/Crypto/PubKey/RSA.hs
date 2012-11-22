af171192496ac065ec9a58fb321c6f129e7c2523
Bill Moyers <bill.c.moyers@gmail.com>
2012-11-22 14:29:41 -0500

    Starting towards byte arrays.

--- src/Crypto/PubKey/RSA.hs
+++ src/Crypto/PubKey/RSA.hs
@@ -1,6 +1,10 @@
 module Crypto.PubKey.RSA where
 import Numeric
+import Data.Binary
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as BL
 
+-- Types
 data PublicKey = PublicKey Integer Integer
 
 showPublicKey :: PublicKey -> String
@@ -14,9 +18,6 @@ showPrivateKey :: PrivateKey -> String
 showPrivateKey (PrivateKey n d) = "PrivateKey(n=" ++ (show n) ++ ", d=" ++ (show d) ++ ")"
 
 instance Show PrivateKey where show = showPrivateKey
-
-totient :: Integer -> Integer -> Integer
-totient p q = (p - 1) * (q - 1)
 
 -- Primality test.
 prime :: Integer -> Bool
@@ -38,6 +39,9 @@ moduloMultiplicativeInverse a b =
 			in (t, s - (q * t))
 
 -- Generate a key pair.
+totient :: Integer -> Integer -> Integer
+totient p q = (p - 1) * (q - 1)
+
 generateKeyPair :: (PublicKey, PrivateKey)
 generateKeyPair = (PublicKey n e, PrivateKey n d)
 	where
@@ -47,8 +51,8 @@ generateKeyPair = (PublicKey n e, PrivateKey n d)
 		phi = totient p q
 		coprimes = (primesInRange 17 phi)
 		e = (take 1 coprimes) !! 0
-		mmi = moduloMultiplicativeInverse e phi
-		d = phi + fst mmi
+		mmi = fst $ moduloMultiplicativeInverse e phi
+		d = if mmi < 0 then phi + mmi else mmi
 
 encrypt :: PublicKey -> Integer -> Integer
 encrypt (PublicKey n e) c = (c ^ e) `mod` n
@@ -56,12 +60,19 @@ encrypt (PublicKey n e) c = (c ^ e) `mod` n
 decrypt :: PrivateKey -> Integer -> Integer
 decrypt (PrivateKey n d) c = (c ^ d) `mod` n
 
+prettyPrint :: B.ByteString -> String
+prettyPrint = concat . map (flip showHex "") . B.unpack
+
+toStrict :: BL.ByteString -> B.ByteString
+toStrict = B.concat . BL.toChunks
+
 main = do
 	putStrLn $ show pub
 	putStrLn $ show priv
 	putStrLn $ show plain
 	putStrLn $ show cipher
 	putStrLn $ show decrypted
+	putStrLn $ show $ prettyPrint $ toStrict (encode decrypted)
 	where
 		(pub, priv) = generateKeyPair
 		plain = 65
