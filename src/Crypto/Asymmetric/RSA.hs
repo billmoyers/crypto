5c36bcd3b448c9799ca4210917ed53c885a01f10
Bill Moyers <bill.c.moyers@gmail.com>
2012-11-28 22:17:52 -0500

    Just cleaning up some things.

--- src/Crypto/Asymmetric/RSA.hs
+++ src/Crypto/Asymmetric/RSA.hs
@@ -12,7 +12,7 @@ import qualified Data.ByteString.Base64 as B64
 import Crypto.Cipher
 
 showBase64 :: Integer -> String
-showBase64 i = show $ B64.encode $ toStrict $ encode i
+showBase64 i = show $ B64.encode $ strictEncode i
 
 -- Types
 data PublicKey = PublicKey Integer Integer
@@ -35,28 +35,26 @@ instance Decipher PrivateKey where decipher = decipher'
 primesInRange :: Integer -> Integer -> [Integer]
 primesInRange a b = filter isPrime [a..b]
 
--- Using Extended Euclidean Algorithm, returning the multiplicative inverses
-moduloMultiplicativeInverse :: Integer -> Integer -> (Integer, Integer)
-moduloMultiplicativeInverse a b = 
+-- Using Extended Euclidean Algorithm, returning the modulo multiplicative inverses.
+modmulinv :: Integer -> Integer -> (Integer, Integer)
+modmulinv a b = 
 	case b of
 		0 -> (1, 0)
 		_ -> let 
 				q = a `quot` b
 				r = a `rem` b
-				(s, t) = moduloMultiplicativeInverse b r
+				(s, t) = modmulinv b r
 			in (t, s - (q * t))
 
 -- Generate a key pair.
-totient :: Integer -> Integer -> Integer
-totient p q = (p - 1) * (q - 1)
-
 generatePrimes :: StdGen -> Int -> (Integer, Integer)
 generatePrimes stdGen bits = (p, q)
 	where
 		bitsp = (bits + 1) `quot` 2
 		bitsq = bits - bitsp
 		m = shiftL 1 (bits - 1)
-		candidates = map (\x -> ((x .|. 1) .|. m)) (randomRs (3, m) stdGen)
+		setLowAndHighBits x = ((x .|. 1) .|. m)
+		candidates = map setLowAndHighBits (randomRs (3, m) stdGen)
 		primes = filter isPrime candidates
 		p = primes !! 0
 		q = primes !! 1
@@ -66,13 +64,14 @@ generateKeyPair stdGen bits =
 	let
 		(p, q) = generatePrimes stdGen bits
 		n = p * q
-		phi = totient p q
+		phi = (p - 1) * (q - 1)
 		coprimes = (primesInRange 17 phi)
 		e = (take 1 coprimes) !! 0
-		mmi = fst $ moduloMultiplicativeInverse e phi
+		mmi = fst $ modmulinv e phi
 		d = if mmi < 0 then phi + mmi else mmi
 	in (PublicKey n e, PrivateKey n d)
 
+-- Modular exponentiation.
 modexp :: forall a. Integral a => a -> a -> a -> a
 modexp b e n = modexp' 1 b e
     where
